86919bbc0cc0faa6689a3905ac44aff71321c3af
luanwcs84 <124096843+luanwcs84@users.noreply.github.com>
2026-03-20 10:34:16 -0300

    Criação da tela de cadastro

--- app/src/main/java/com/example/reporteurbano/CadastroActivity.java
+++ app/src/main/java/com/example/reporteurbano/CadastroActivity.java
@@ -1,6 +1,8 @@
 package com.example.reporteurbano;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,14 @@ public class CadastroActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView btnVoltar = findViewById(R.id.txtVoltarLogin);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
