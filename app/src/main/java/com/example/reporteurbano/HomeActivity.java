166e732105bff4e683797e461788d9ed15731e3a
luanwcs84 <124096843+luanwcs84@users.noreply.github.com>
2026-03-21 20:34:43 -0300

    feat: criação da tela de Novo Reporte

--- app/src/main/java/com/example/reporteurbano/HomeActivity.java
+++ app/src/main/java/com/example/reporteurbano/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.reporteurbano;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -52,7 +53,9 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         btnNovoReporte.setOnClickListener(v -> {
-            Toast.makeText(this, "Iniciando novo reporte...", Toast.LENGTH_SHORT).show();
+            // Cria a intenção de ir da Home para o NovoReporte
+            Intent intent = new Intent(HomeActivity.this, NovoReporteActivity.class);
+            startActivity(intent);
         });
     }
 }
