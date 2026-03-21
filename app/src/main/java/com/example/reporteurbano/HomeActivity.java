4b021ca6b7257a5433597d460ac5191a1899b82f
luanwcs84 <124096843+luanwcs84@users.noreply.github.com>
2026-03-21 17:39:02 -0300

    feat: criação do menu lateral

--- app/src/main/java/com/example/reporteurbano/HomeActivity.java
+++ app/src/main/java/com/example/reporteurbano/HomeActivity.java
@@ -1,24 +1,58 @@
 package com.example.reporteurbano;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private MaterialToolbar toolbar;
+    private FloatingActionButton btnNovoReporte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbarHome);
+        btnNovoReporte = findViewById(R.id.btnNovoReporte);
+        setSupportActionBar(toolbar);
+
+        ViewCompat.setOnApplyWindowInsetsListener(drawerLayout, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        toolbar.setNavigationOnClickListener(v -> {
+            drawerLayout.open();
+        });
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_sair) {
+                finish();
+            }
+            drawerLayout.close();
+            return true;
+        });
+
+        btnNovoReporte.setOnClickListener(v -> {
+            Toast.makeText(this, "Iniciando novo reporte...", Toast.LENGTH_SHORT).show();
         });
     }
 }
